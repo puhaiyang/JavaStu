8b62ccf3cdd7327fd0474c5dada35664e3218274
puhaiyang <761396462@qq.com>
2019-03-15 15:37:27 +0800

    1.INIT

---
+++ spring-transaction-manager/src/main/java/com/haiyang/javastu/springtransactionmanager/SpringTransactionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.haiyang.javastu.springtransactionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionManagerApplication.class, args);
+	}
+
+}
